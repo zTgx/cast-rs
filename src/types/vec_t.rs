437e9c5dbb5a7245a2d64ac6d8b15c15de24cc4b
beautifularea <beautifularea@gmail.com>
2019-07-04 11:53:49 +0800

    update examples

--- src/types/vec_t.rs
+++ src/types/vec_t.rs
@@ -5,13 +5,8 @@ pub fn to_str<'a> (v: &'a Vec<u8>) -> Result<&'a str, Utf8Error> {
     str::from_utf8(&v)
 }
 
-pub fn to_string(v: Vec<u8>) -> String {
-    let s: String = v.iter().map(|c| {
-            let x = format!("{:x}", c);
-            x
-            }).collect();
-
-    s
+pub fn to_string<'a> (v: &'a Vec<u8>) -> String {
+    v.iter().map(|c| { *c as char }).collect::<String>()
 }
 
 pub fn to_array<'a> (v: &'a Vec<u8>) -> &'a [u8] {
