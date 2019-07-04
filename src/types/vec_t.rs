d4f445f40878074a80624a3a2de73ceb48edf9fa
beautifularea <beautifularea@gmail.com>
2019-07-04 16:39:04 +0800

    update vec.rs

--- src/types/vec_t.rs
+++ src/types/vec_t.rs
@@ -13,3 +13,30 @@ pub fn to_array<'a> (v: &'a Vec<u8>) -> &'a [u8] {
     v.as_slice()
 }
 
+pub fn to_1_array<'a> (v: &'a Vec<u8>) -> Option<[u8; 1]> {
+    if v.len() != 1 { return None; }
+    let mut x: [u8; 1] = [0u8; 1];
+    let mut index = 0;
+    for &item in v.iter() {
+        x[index] = item;
+
+        index += 1;
+    }
+
+    Some(x)
+}
+
+pub fn to_8_array<'a> (v: &'a Vec<u8>) -> Option<[u8; 8]> {
+    if v.len() != 8 { return None; }
+    let mut x: [u8; 8] = [0u8; 8];
+    let mut index = 0;
+    for &item in v.iter() {
+        x[index] = item;
+
+        index += 1;
+    }
+
+    Some(x)
+}
+
+
