8e4ab00fcab4cad7fc236530865ed991a5f90365
zTgx <747674262@qq.com>
2019-08-08 11:21:04 +0800

    add downcast-rs

---
+++ examples/downcast.rs
@@ -0,0 +1,38 @@
+// Import macro via `macro_use` pre-1.30.
+
+extern crate cast_rs;
+use cast_rs::downcast::*;
+
+// To create a trait with downcasting methods, extend `Downcast` and run
+// `impl_downcast!()` on the trait.
+trait Base: Downcast {}
+impl_downcast!(Base);
+
+// Concrete types implementing Base.
+#[derive(Debug)]
+struct Foo(u32);
+impl Base for Foo {}
+#[derive(Debug)]
+struct Bar(f64);
+impl Base for Bar {}
+
+fn main() {
+    // Create a trait object.
+    let base: Box<Base> = Box::new(Foo(42));
+
+    // Try sequential downcasts.
+    if let Some(foo) = base.downcast_ref::<Foo>() {
+        assert_eq!(foo.0, 42);
+    } else if let Some(bar) = base.downcast_ref::<Bar>() {
+        assert_eq!(bar.0, 42.0);
+    }
+
+    assert!(base.is::<Foo>());
+
+    // Fail to convert `Box<Base>` into `Box<Bar>`.
+    let res = base.downcast::<Bar>();
+    assert!(res.is_err());
+    let base = res.unwrap_err();
+    // Convert `Box<Base>` into `Box<Foo>`.
+    assert_eq!(42, base.downcast::<Foo>().map_err(|_| "Shouldn't happen.").unwrap().0);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,9 @@ pub use crate::types::hex_t     as hex_t;
 pub use crate::types::str_t     as str_t;
 pub use crate::types::string_t  as string_t;
 pub use crate::types::vec_t     as vec_t;
+
+#[macro_use]
+extern crate downcast_rs;
+pub mod downcast {
+     pub use downcast_rs::*;
+}
